a8414f968d446e57b6c610b468c7e5c58236da89
Jibon <manvirjibon@gmail.com>
2024-02-11 20:09:54 +0600

    cityName query

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const submitBtn = document.querySelector('.submit-btn');
 const input = document.querySelector('.input');
-
+const cityName = document.querySelector('.cityName')
 submitBtn.addEventListener('click', function(){
     const input = document.querySelector('.input').value
-      
+
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+input+'&appid=92e16a89df89783767314ac812be80ce')
     .then(response => response.json())
     .then(data=>{
