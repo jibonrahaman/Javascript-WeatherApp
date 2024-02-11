a367e4725e4cd8522e4c51961e4f839b212dfb34
Jibon <manvirjibon@gmail.com>
2024-02-11 20:07:02 +0600

    data fetch

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const submitBtn = document.getElementsByClassName('submit-btn');
+const submitBtn = document.querySelector('.submit-btn');
+const input = document.querySelector('.input');
+
+submitBtn.addEventListener('click', function(){
+    const input = document.querySelector('.input').value
+      
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input+'&appid=92e16a89df89783767314ac812be80ce')
+    .then(response => response.json())
+    .then(data=>{
+        console.log(data)
+    })
+});
