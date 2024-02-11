b21921b683b594266415a29cd0e44a9958045b7a
Jibon <manvirjibon@gmail.com>
2024-02-11 20:24:16 +0600

    done

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ submitBtn.addEventListener('click', function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+input+'&appid=92e16a89df89783767314ac812be80ce')
     .then(response => response.json())
     .then(data=>{
-        console.log(data)
+        
+        const Temp = data.main.temp;
+        const tempareture = (Temp - 273.15).toFixed(2)
+        
+
+        cityName.innerHTML = data.name;
+        tem.innerHTML = tempareture;
+        des.innerHTML = data.weather[0].description
     })
 });
